a77a17b547c3e39fa2692043530249cc12189e2d
tomtch <tomtch7@gmail.com>
2025-01-08 16:29:07 +0100

    turn off sensor LP filters for EKF

--- FlightController/src/config.h
+++ FlightController/src/config.h
@@ -20,19 +20,21 @@
 // Settings
 #define SAFETY 1  // set safe mode
 #define TELEMETRY 1 
-#define DEBUG 0 // enable debugging messages
+#define DEBUG 1 // enable debugging messages
 
 // Timeout settings
-#define COMMAND_TIMEOUT_MS 60000  // 60000 ms
+#define COMMAND_TIMEOUT_MS 60000   // 60000 ms
 #define SENSOR_TIMEOUT_US 2000  // 2 ms
 
 // filter constants
 #define GYROLPF_RATIO 1.0f  // used to weight the new measurement
-#define ACCLPF_RATIO 0.01f  // used to weight the new measurement
-#define MAGLPF_RATIO 0.05f  // used to weight the new measurement
+// #define ACCLPF_RATIO 0.01f  // used to weight the new measurement
+// #define MAGLPF_RATIO 0.05f  // used to weight the new measurement
+#define CF_RATIO 0.95f  // gyro weight in complementary filter
+#define ACCLPF_RATIO 1.0f  // used to weight the new measurement EKF
+#define MAGLPF_RATIO 1.0f  // used to weight the new measurement EKF
 #define ALTLPF_RATIO 0.05f  // used to weight the new measurement
 #define BATLPF_RATIO 0.1f  // used to weight the new measurement
-#define CF_RATIO 0.95f  // gyro weight in complementary filter
 
 // Some approximate measurements
  #define MOTOR_FORCE 680.0f  // youtube approx value
