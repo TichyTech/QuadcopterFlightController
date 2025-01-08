696870c951db95a777364770d52d6c6b85da69b8
tomtch <tomtch7@gmail.com>
2025-01-08 16:30:26 +0100

    remove debugging clutter

--- FlightController/src/main.cpp
+++ FlightController/src/main.cpp
@@ -21,10 +21,11 @@
 Measurements measured_values;  // struct to store all measured values
 Matrix3 estimated_DCM;  // matrix to store current estimated attitude
 State current_state;
+Vector4 q;  // current attitude quaternion
 Control commands;
 Vector4 control_action; 
 
-unsigned long start_time = 0;
+uint32_t start_time = 0;
 float initial_yaw = 0;
 uint32_t motors_on = 0;
 
@@ -67,7 +68,14 @@ void setup() {
 
   arm_motors();  // send non-zero throttle to the motors to arm them
   measured_values = sensors.get_measurements_filtered();
-  
+  Serial.println("Init measurements: Gyro ");
+  printVec3(measured_values.gyro_vec, 2);
+  Serial.println("ACC ");
+  printVec3(measured_values.acc_vec, 2);
+  Serial.println("MAG ");
+  printVec3(measured_values.mag_vec, 2);
+  Serial.println();
+
   if (SAFETY){
     while (sensors.imu_timed_out){
       Serial.println("IMU timeout");
@@ -82,6 +90,8 @@ void setup() {
   current_state = compute_state(estimated_DCM, measured_values);  // initial state for reference
   initial_yaw = current_state.yaw;  // store initial yaw for control
   start_time = millis();
+
+  gyro_avg = measured_values.gyro_vec;
 }
 
 void setup1(){
@@ -89,9 +99,6 @@ void setup1(){
       delay(200);
   }
 }
-
-uint16_t loop_counter = 0;
-Vector3 gyro_avg = {0,0,0};
 
 void loop() {  // approxx 0.85 ms per loop 
   while(digitalRead(SWITCH_PIN)) {  // stop motors and blink red LED if switch is on
@@ -140,42 +147,14 @@ void loop() {  // approxx 0.85 ms per loop
   // estimated_DCM = acc_mag2DCM(measured_values); // init DCM
   // estimated_DCM = update_DCM_rejection(estimated_DCM, measured_values);  // update DCM matrix
   // controller.update_DCM(estimated_DCM);
-  Vector4 q;
-  q = k_filter.predict(measured_values.gyro_vec, measured_values.integration_period);
-  gyro_avg = 0.995f*gyro_avg + 0.005f*measured_values.gyro_vec;
+  q = k_filter.predict(measured_values.gyro_vec, measured_values.integration_period);  // model prediction using gyro
+  k_filter.track_acc(measured_values.acc_vec, measured_values.integration_period);  // track accelerometer magnitude
+  q = k_filter.fuse_acc(normalize(measured_values.acc_vec));  // fuse accelerometer data
+  q = k_filter.fuse_mag(normalize(measured_values.mag_vec));  // fuse magnetometer data
+  estimated_DCM = k_filter.quat2R(q);  // quat to DCM conversion
 
-  if (loop_counter == 0){
-    Serial.println(millis() - start_time);
-    Serial.print("gyro: ");
-    printVec3(measured_values.gyro_vec, 4);
-    Serial.println();
-    Serial.print("gyro_avg: ");
-    printVec3(gyro_avg, 4);
-    Serial.println();
-    Serial.print("predicted: ");
-    printVec4(q, 4);
-    Serial.println();
-  }
-  q = k_filter.fuse_acc(normalize(measured_values.acc_vec));
-  q = k_filter.fuse_mag(normalize(measured_values.acc_vec), normalize(measured_values.mag_vec));
-  if (loop_counter == 0){
-    Serial.print("acc: ");
-    printVec3(measured_values.acc_vec, 4);
-    Serial.println();
-    Serial.print("mag: ");
-    printVec3(normalize(measured_values.mag_vec), 4);
-    Serial.println();
-    Serial.print("corrected: ");
-    printVec4(q, 4);
-    Serial.println();
-    loop_counter = 0;
-  }
-  loop_counter ++;
-  if (loop_counter = 1) loop_counter = 0;
-  estimated_DCM = k_filter.quat2R(q);
-  controller.update_DCM(estimated_DCM);
-  control_action = controller.update_motor_percentages(ctrl_commands, measured_values);
-
+  controller.update_DCM(estimated_DCM);  
+  control_action = controller.update_motor_percentages(ctrl_commands, measured_values); 
   if (motors_on == 0) signal_motors(zero_4vector); 
   else signal_motors(control_action);
 }
